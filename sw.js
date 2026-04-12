59405c759c38aeb20a0d1478589103e7cef187f7
colsensus <walterkgall@outlook.com>
2026-04-12 23:14:59 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
 const CACHE_NAME = 'orion_app';
-
-// install → cache app shell
+const ACACHE = 'api-cache';
+const CACHE_LIST = [
+ '/',
+'/favicon.png',
+'/index.html',
+'/manifest.json',
+'/sw.js'  
+  ];
+  
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        '/',
-        '/favicon.png',
-        '/index.html',
-        '/manifest.json',
-        '/sw.js'
-      ]);
+
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(CACHE_LIST);
     })
   );
 });
@@ -21,39 +23,36 @@ self.addEventListener('activate', e => {
 });
 
 // fetch handler (PROXY + CACHE)
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch',event => {
 const url = new URL(event.request.url);
-
-  // proxy endpoint
-  if (url.pathname === '/proxy') {
-const target = url.searchParams.get('url');
-event.respondWith(proxyFetch(target));
+if (url.pathname === '/proxy'){
+const request=url.searchParams.get('url');
+event.respondWith(proxyFetch(request));
     return;
   }
-
-  // normal caching
+if (url.pathname.endsWith('/')) {
   event.respondWith(
-    caches.match(event.request).then(res => {
-  return res || fetch(event.request);
-    })
-  );
+  caches.match('/index.html').then(res =>{return res||fetch('/index.html');
+      }));
+return;
+}
+event.respondWith(
+caches.match(event.request).then(res =>{
+return res || fetch(event.request)}));
 });
 
 // proxy function
-async function proxyFetch(target) {
-  const cache = await caches.open('api-cache');
+async function proxyFetch(request) {
+const cache=await caches.open(ACACHE);
 
   try {
-  const response =await fetch(target);
-
+  const response =await fetch(request);
     const clone = response.clone();
-    cache.put(target, clone);
-
+    cache.put(request, clone);
     return response;
-
   } catch (e) {
     // offline fallback
-    const cached = await cache.match(target);
+    const cached = await cache.match(request);
     return cached || new Response(JSON.stringify({ error: 'offline' }), {
       headers: { 'Content-Type': 'application/json' }
     });
